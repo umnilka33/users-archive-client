e1d97465b9129f1923d9f03421349bb353153152
umnilka33 <umnmilka33@gmail.com>
2023-08-24 19:47:13 +0300

    change funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import TabsMenu from './components/TabsMenu/TabsMenu';
-import { IUserActionTypes } from './types/types';
 
 function App() {
-const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch({type: IUserActionTypes.GET_USERS})
-// eslint-disable-next-line react-hooks/exhaustive-deps
-},[])
   return (
     <div className="App">
       <TabsMenu/>
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "@mui/material";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../redux/Users";
 import { IUserActionTypes } from "../../types/types";
@@ -8,15 +9,19 @@ export const UserList = () => {
     const usersArray = useSelector(getUsers);
     const dispatch = useDispatch();
     const handleDeleteUser = (id: string) => {
-        dispatch({type: IUserActionTypes.DELETE_USER, payload: id})
+        dispatch({ type: IUserActionTypes.DELETE_USER, payload: id })
     }
 
+    useEffect(() => {
+        dispatch({ type: IUserActionTypes.GET_USERS })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
-        <Stack gap="30px" direction="row" flexWrap="wrap" sx={{width: '810px'}}>
-            {usersArray.map((item)=>
-            <UserCard key={item.id} user={item} onDelete={handleDeleteUser}/>
+        <Stack gap="30px" direction="row" flexWrap="wrap" sx={{ width: '810px' }}>
+            {usersArray.map((item) =>
+                <UserCard key={item.id} user={item} onDelete={handleDeleteUser} />
             )}
-            {usersArray.length?null:'Нет пользователей'}
+            {usersArray.length ? null : 'Нет пользователей'}
         </Stack>
     )
 }
